979e4d1dd199cdc2c5d94c5e685d8e28f4859707
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2023-05-26 17:44:33 +0200

    added more zeta and golombs to codes stats

--- src/codes/stats.rs
+++ src/codes/stats.rs
@@ -15,9 +15,13 @@ pub struct CodesStats {
     pub zeta2: usize,
     pub zeta3: usize,
     pub zeta4: usize,
+    pub zeta5: usize,
+    pub zeta6: usize,
     pub golomb2: usize,
     pub golomb3: usize,
     pub golomb4: usize,
+    pub golomb5: usize,
+    pub golomb6: usize,
 }
 
 impl CodesStats {
@@ -32,12 +36,18 @@ impl CodesStats {
         self.unary = self.unary.saturating_add(len_unary(value));
         self.gamma = self.gamma.saturating_add(len_gamma(value));
         self.delta = self.delta.saturating_add(len_delta(value));
+
         self.zeta2 = self.zeta2.saturating_add(len_zeta(value, 2));
         self.zeta3 = self.zeta3.saturating_add(len_zeta(value, 3));
         self.zeta4 = self.zeta4.saturating_add(len_zeta(value, 4));
+        self.zeta5 = self.zeta5.saturating_add(len_zeta(value, 5));
+        self.zeta6 = self.zeta6.saturating_add(len_zeta(value, 6));
+
         self.golomb2 = self.golomb2.saturating_add(len_golomb(value, 2));
         self.golomb3 = self.golomb3.saturating_add(len_golomb(value, 3));
         self.golomb4 = self.golomb4.saturating_add(len_golomb(value, 4));
+        self.golomb5 = self.golomb5.saturating_add(len_golomb(value, 5));
+        self.golomb6 = self.golomb6.saturating_add(len_golomb(value, 6));
         value
     }
     /// Return the best code for the stream, as in the one that needed the
@@ -47,45 +57,29 @@ impl CodesStats {
         let mut best = self.unary;
         let mut best_code = Code::Unary;
 
-        if self.gamma < best {
-            best = self.gamma;
-            best_code = Code::Gamma;
+        macro_rules! check {
+            ($code:expr, $len:expr) => {
+                if $len < best {
+                    best = $len;
+                    best_code = $code;
+                }
+            };
         }
 
-        if self.delta < best {
-            best = self.delta;
-            best_code = Code::Delta;
-        }
+        check!(Code::Gamma, self.gamma);
+        check!(Code::Delta, self.delta);
 
-        if self.zeta2 < best {
-            best = self.zeta2;
-            best_code = Code::Zeta { k: 2 };
-        }
+        check!(Code::Zeta { k: 2 }, self.zeta2);
+        check!(Code::Zeta { k: 3 }, self.zeta3);
+        check!(Code::Zeta { k: 4 }, self.zeta4);
+        check!(Code::Zeta { k: 5 }, self.zeta5);
+        check!(Code::Zeta { k: 6 }, self.zeta6);
 
-        if self.zeta3 < best {
-            best = self.zeta3;
-            best_code = Code::Zeta { k: 3 };
-        }
-
-        if self.zeta4 < best {
-            best = self.zeta4;
-            best_code = Code::Zeta { k: 4 };
-        }
-
-        if self.golomb2 < best {
-            best = self.golomb2;
-            best_code = Code::Golomb { b: 2 };
-        }
-
-        if self.golomb3 < best {
-            best = self.golomb3;
-            best_code = Code::Golomb { b: 3 };
-        }
-
-        if self.golomb4 < best {
-            best = self.golomb4;
-            best_code = Code::Golomb { b: 4 };
-        }
+        check!(Code::Golomb { b: 2 }, self.golomb2);
+        check!(Code::Golomb { b: 3 }, self.golomb3);
+        check!(Code::Golomb { b: 4 }, self.golomb4);
+        check!(Code::Golomb { b: 5 }, self.golomb5);
+        check!(Code::Golomb { b: 6 }, self.golomb6);
 
         (best_code, best)
     }
