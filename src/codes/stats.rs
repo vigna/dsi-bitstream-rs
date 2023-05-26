63b4b47b2a572cfebeba21922e749c1cc1a560b9
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2023-05-26 17:49:17 +0200

    refactored zetas and golombs into an array instead of having multiple variables

--- src/codes/stats.rs
+++ src/codes/stats.rs
@@ -12,16 +12,8 @@ pub struct CodesStats {
     pub unary: usize,
     pub gamma: usize,
     pub delta: usize,
-    pub zeta2: usize,
-    pub zeta3: usize,
-    pub zeta4: usize,
-    pub zeta5: usize,
-    pub zeta6: usize,
-    pub golomb2: usize,
-    pub golomb3: usize,
-    pub golomb4: usize,
-    pub golomb5: usize,
-    pub golomb6: usize,
+    pub zeta: [usize; 10],
+    pub golomb: [usize; 10],
 }
 
 impl CodesStats {
@@ -37,17 +29,12 @@ impl CodesStats {
         self.gamma = self.gamma.saturating_add(len_gamma(value));
         self.delta = self.delta.saturating_add(len_delta(value));
 
-        self.zeta2 = self.zeta2.saturating_add(len_zeta(value, 2));
-        self.zeta3 = self.zeta3.saturating_add(len_zeta(value, 3));
-        self.zeta4 = self.zeta4.saturating_add(len_zeta(value, 4));
-        self.zeta5 = self.zeta5.saturating_add(len_zeta(value, 5));
-        self.zeta6 = self.zeta6.saturating_add(len_zeta(value, 6));
-
-        self.golomb2 = self.golomb2.saturating_add(len_golomb(value, 2));
-        self.golomb3 = self.golomb3.saturating_add(len_golomb(value, 3));
-        self.golomb4 = self.golomb4.saturating_add(len_golomb(value, 4));
-        self.golomb5 = self.golomb5.saturating_add(len_golomb(value, 5));
-        self.golomb6 = self.golomb6.saturating_add(len_golomb(value, 6));
+        for (k, val) in self.zeta.iter_mut().enumerate() {
+            *val = val.saturating_add(len_zeta(value, (k + 1) as _));
+        }
+        for (b, val) in self.golomb.iter_mut().enumerate() {
+            *val = val.saturating_add(len_golomb(value, (b + 1) as _));
+        }
         value
     }
     /// Return the best code for the stream, as in the one that needed the
@@ -69,17 +56,12 @@ impl CodesStats {
         check!(Code::Gamma, self.gamma);
         check!(Code::Delta, self.delta);
 
-        check!(Code::Zeta { k: 2 }, self.zeta2);
-        check!(Code::Zeta { k: 3 }, self.zeta3);
-        check!(Code::Zeta { k: 4 }, self.zeta4);
-        check!(Code::Zeta { k: 5 }, self.zeta5);
-        check!(Code::Zeta { k: 6 }, self.zeta6);
-
-        check!(Code::Golomb { b: 2 }, self.golomb2);
-        check!(Code::Golomb { b: 3 }, self.golomb3);
-        check!(Code::Golomb { b: 4 }, self.golomb4);
-        check!(Code::Golomb { b: 5 }, self.golomb5);
-        check!(Code::Golomb { b: 6 }, self.golomb6);
+        for (k, val) in self.zeta.iter().enumerate() {
+            check!(Code::Zeta { k: (k + 1) as _ }, *val);
+        }
+        for (b, val) in self.golomb.iter().enumerate() {
+            check!(Code::Golomb { b: (b + 1) as _ }, *val);
+        }
 
         (best_code, best)
     }
