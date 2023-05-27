b279bfafd1462ab95951ed00df22dba111e18116
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2023-05-27 23:22:39 +0200

    Added mod.rs

---
+++ src/utils/mod.rs
@@ -0,0 +1,13 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Sebastiano Vigna
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR LGPL-2.1-or-later
+ */
+
+//! # Traits
+//! This modules contains the traits that are used throughout the crate.
+//! They are collected into a module so you can do `use dsi_bitstream::utils::*;`
+//! for ease of use.
+
+mod count;
+pub use count::*;
