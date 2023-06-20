660614d7fa67f027668afbde95b7df5edc433a88
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2023-06-20 11:05:40 +0200

    made CodesStats atomic

--- src/codes/stats.rs
+++ src/codes/stats.rs
@@ -1,19 +1,20 @@
 use super::*;
+use core::sync::atomic::{AtomicUsize, Ordering};
 
 pub fn len_golomb(value: u64, b: u64) -> usize {
     len_unary(value / b) + len_minimal_binary(value % b, b)
 }
 
-#[derive(Default, Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Default, Debug)]
 /// A struct to keep track of the space needed to store a stream of integers
 /// using different codes, this can be used to determine which code is the
 /// most efficient for a given stream.
 pub struct CodesStats {
-    pub unary: usize,
-    pub gamma: usize,
-    pub delta: usize,
-    pub zeta: [usize; 10],
-    pub golomb: [usize; 20],
+    pub unary: AtomicUsize,
+    pub gamma: AtomicUsize,
+    pub delta: AtomicUsize,
+    pub zeta: [AtomicUsize; 10],
+    pub golomb: [AtomicUsize; 20],
 }
 
 impl CodesStats {
@@ -24,16 +25,16 @@ impl CodesStats {
 
     /// Update the stats with the length of the code for `value` and return back
     /// `value` for convienience
-    pub fn update(&mut self, value: u64) -> u64 {
-        self.unary = self.unary.saturating_add(len_unary(value));
-        self.gamma = self.gamma.saturating_add(len_gamma(value));
-        self.delta = self.delta.saturating_add(len_delta(value));
+    pub fn update(&self, value: u64) -> u64 {
+        self.unary.fetch_add(len_unary(value), Ordering::Relaxed);
+        self.gamma.fetch_add(len_gamma(value), Ordering::Relaxed);
+        self.delta.fetch_add(len_delta(value), Ordering::Relaxed);
 
-        for (k, val) in self.zeta.iter_mut().enumerate() {
-            *val = val.saturating_add(len_zeta(value, (k + 1) as _));
+        for (k, val) in self.zeta.iter().enumerate() {
+            val.fetch_add(len_zeta(value, (k + 1) as _), Ordering::Relaxed);
         }
-        for (b, val) in self.golomb.iter_mut().enumerate() {
-            *val = val.saturating_add(len_golomb(value, (b + 1) as _));
+        for (b, val) in self.golomb.iter().enumerate() {
+            val.fetch_add(len_golomb(value, (b + 1) as _), Ordering::Relaxed);
         }
         value
     }
@@ -41,13 +42,14 @@ impl CodesStats {
     /// least space, and the space needed by that code
     pub fn get_best_code(&self) -> (Code, usize) {
         // TODO!: make cleaner
-        let mut best = self.unary;
+        let mut best = self.unary.load(Ordering::Relaxed);
         let mut best_code = Code::Unary;
 
         macro_rules! check {
             ($code:expr, $len:expr) => {
-                if $len < best {
-                    best = $len;
+                let len = $len.load(Ordering::Relaxed);
+                if len < best {
+                    best = len;
                     best_code = $code;
                 }
             };
