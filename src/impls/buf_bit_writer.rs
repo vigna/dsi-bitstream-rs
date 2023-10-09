ab8590485e749563ebd338602543aef94c00e87a
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2023-10-09 09:02:50 +0200

    Completed rewrite, improved tests

--- src/impls/buf_bit_writer.rs
+++ src/impls/buf_bit_writer.rs
@@ -13,31 +13,33 @@ use anyhow::{bail, Result};
 use common_traits::{CastableInto, Integer, Number};
 
 /// An implementation of [`BitWrite`] for a
-/// [`WordWrite`] with word `u64` and of [`BitSeek`] for a [`WordSeek`].
+/// [`WordWrite`] and of [`BitSeek`] for a [`WordSeek`].
 ///
 /// Endianness can be selected using the parameter `E`.
 ///
-/// This implementation uses a
-/// bit buffer to store bits that are not yet written. The type of the bit buffer
-/// is `u128`.
+/// This implementation uses a bit buffer to store bits that are not yet written.
+/// The size of the bit buffer is the size of the word used by the [`WordWrite`].
+///
+/// The additional type parameter `WCP` is used to select the parameters for the
+/// instantanous codes, but the casual user should be happy with the default value.
+/// See [`WriteParams`] for more details.
 
 #[derive(Debug)]
 pub struct BufBitWriter<E: BBSWDrop<WR, WCP>, WR: WordWrite, WCP: WriteParams = DefaultWriteParams>
 {
     /// The [`WordWrite`] to which we will write words.
     backend: WR,
-    /// The buffer where we store bits we have a word worth of them.
-    /// Note that only `bits_in_buffer` bits are valid:
-    /// the rest have undefined value.
+    /// The buffer where we store bits until we have a word worth of them.
+    /// Note that only `bits_in_buffer` bits are valid: the rest have undefined value.
     buffer: WR::Word,
-    /// How many bits in buffer are valid, from zero (included)
-    /// to `WR::Word::BITS` (excluded).
+    /// How many bits in buffer are valid, from zero
+    /// to `WR::Word::BITS`, both included.
     bits_in_buffer: usize,
     _marker_endianness: core::marker::PhantomData<(E, WCP)>,
 }
 
 impl<E: BBSWDrop<WR, WCP>, WR: WordWrite, WCP: WriteParams> BufBitWriter<E, WR, WCP> {
-    /// Create a new [`BufBitWriter`] from a backend word writer
+    /// Create a new [`BufBitWriter`] around a [`WordWrite`].
     pub fn new(backend: WR) -> Self {
         Self {
             backend,
@@ -94,7 +96,7 @@ where
     }
 
     #[inline]
-    fn write_bits(&mut self, mut value: u64, n_bits: usize) -> Result<usize> {
+    fn write_bits(&mut self, value: u64, n_bits: usize) -> Result<usize> {
         if n_bits > 64 {
             bail!("Too many bits: {} > 64", n_bits);
         }
@@ -111,16 +113,16 @@ where
                 // Handling this case algorithmically is a pain
                 return Ok(0);
             }
-            // to_write might be 64 and the buffer might be empty
+            // n_bits might be 64 and the buffer might be empty
             self.buffer = self.buffer << n_bits - 1 << 1;
             self.buffer |= value.cast();
             self.bits_in_buffer += n_bits;
             return Ok(n_bits);
         }
 
-        // Load the bottom of the buffer and dump the whole buffer
-        self.buffer <<= space_left_in_buffer;
-        if n_bits - space_left_in_buffer != u64::BITS as _ {
+        // Load the bottom of the buffer, if necessary, and dump the whole buffer
+        if space_left_in_buffer != 0 {
+            self.buffer <<= space_left_in_buffer;
             self.buffer |= (value >> n_bits - space_left_in_buffer).cast();
         }
         self.backend.write_word(self.buffer.to_be())?;
@@ -128,11 +130,9 @@ where
         let mut to_write = n_bits - space_left_in_buffer;
 
         for _ in 0..to_write / WR::Word::BITS as usize {
-            self.backend
-                .write_word((value >> to_write - WR::Word::BITS).cast().to_be())?;
-            // Note that since the buffer cannot be full, this instruction
-            // cannot be executed when WR::Word == u64.
             to_write -= WR::Word::BITS;
+            self.backend
+                .write_word((value >> to_write).cast().to_be())?;
         }
 
         self.bits_in_buffer = to_write;
@@ -150,30 +150,31 @@ where
         }
 
         let code_length = value + 1;
-        let space_left = self.space_left_in_buffer() as u64;
+        let space_left_in_buffer = self.space_left_in_buffer() as u64;
 
-        if code_length <= space_left {
+        // Easy way out: we fit the buffer
+        if code_length <= space_left_in_buffer {
             self.bits_in_buffer += code_length as usize;
             self.buffer = self.buffer << value << 1; // Might be code_length == WR::Word::BITS
             self.buffer |= WR::Word::ONE;
             return Ok(code_length as usize);
         }
 
-        if space_left != 0 {
-            self.buffer = self.buffer << space_left - 1 << 1;
+        if space_left_in_buffer == WR::Word::BITS as _ {
+            self.backend.write_word(WR::Word::ZERO)?;
+        } else {
+            self.buffer = self.buffer << space_left_in_buffer;
+            self.backend.write_word(self.buffer.to_be())?;
         }
-        self.backend.write_word(self.buffer.to_be())?;
 
-        value -= space_left;
+        value -= space_left_in_buffer;
 
         for _ in 0..value / WR::Word::BITS as u64 {
             self.backend.write_word(WR::Word::ZERO)?;
         }
 
-        value %= WR::Word::BITS as u64;
-
         self.buffer = WR::Word::ONE;
-        self.bits_in_buffer = value as usize + 1;
+        self.bits_in_buffer = (value % WR::Word::BITS as u64) as usize + 1;
         Ok(code_length as usize)
     }
 
@@ -227,9 +228,9 @@ where
             return Ok(n_bits);
         }
 
-        // Load the top of the buffer and dump the whole buffer
-        self.buffer >>= space_left_in_buffer;
-        if self.bits_in_buffer != WR::Word::BITS {
+        // Load the top of the buffer, if necessary, and dump the whole buffer
+        if space_left_in_buffer != 0 {
+            self.buffer >>= space_left_in_buffer;
             self.buffer |= value.cast() << self.bits_in_buffer;
         }
         self.backend.write_word(self.buffer.to_le())?;
@@ -260,21 +261,24 @@ where
         }
 
         let code_length = value + 1;
-        let space_left = self.space_left_in_buffer() as u64;
+        let space_left_in_buffer = self.space_left_in_buffer() as u64;
 
-        if code_length <= space_left {
+        // Easy way out: we fit the buffer
+        if code_length <= space_left_in_buffer {
             self.bits_in_buffer += code_length as usize;
-            self.buffer = self.buffer >> value >> 1;
+            self.buffer = self.buffer >> value >> 1; // Might be code_length == WR::Word::BITS
             self.buffer |= WR::Word::ONE << WR::Word::BITS - 1;
             return Ok(code_length as usize);
         }
 
-        if space_left != 0 {
-            self.buffer = self.buffer >> space_left - 1 >> 1;
+        if space_left_in_buffer == WR::Word::BITS as _ {
+            self.backend.write_word(WR::Word::ZERO)?;
+        } else {
+            self.buffer = self.buffer >> space_left_in_buffer;
+            self.backend.write_word(self.buffer.to_le())?;
         }
-        self.backend.write_word(self.buffer.to_le())?;
 
-        value -= space_left;
+        value -= space_left_in_buffer;
 
         for _ in 0..value / WR::Word::BITS as u64 {
             self.backend.write_word(WR::Word::ZERO)?;
@@ -293,7 +297,7 @@ where
 }
 
 macro_rules! test_buf_bit_writer {
-    ($f: ident, $bb: ty, $word:ty) => {
+    ($f: ident, $word:ty) => {
         #[test]
         fn $f() -> Result<(), anyhow::Error> {
             use super::MemWordWriterVec;
@@ -357,8 +361,8 @@ macro_rules! test_buf_bit_writer {
             drop(big);
             drop(little);
 
-            type ReadWord = $word;
-            type ReadBuffer = $bb;
+            type ReadWord = u16;
+            type ReadBuffer = u32;
             let be_trans: &[ReadWord] = unsafe {
                 core::slice::from_raw_parts(
                     buffer_be.as_ptr() as *const ReadWord,
@@ -413,6 +417,9 @@ macro_rules! test_buf_bit_writer {
     };
 }
 
-test_buf_bit_writer!(test_u128_u64, u128, u64);
-test_buf_bit_writer!(test_u64_u32, u64, u32);
-test_buf_bit_writer!(test_u32_u16, u32, u16);
+test_buf_bit_writer!(test_u128, u128);
+test_buf_bit_writer!(test_u64, u64);
+test_buf_bit_writer!(test_u32, u32);
+
+test_buf_bit_writer!(test_u16, u16);
+test_buf_bit_writer!(test_usize, usize);
