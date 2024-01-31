aea05e73090becd08530be4380a28a2139c5f341
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2024-01-31 23:05:32 +0100

    write_unary fast

--- benches/write_unary.rs
+++ benches/write_unary.rs
@@ -4,11 +4,12 @@ use rand::rngs::SmallRng;
 use rand::RngCore;
 use rand::SeedableRng;
 use std::hint::black_box;
+use std::time::Duration;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let v_le = <Vec<u64>>::with_capacity(1000000000);
+    let v_le = <Vec<u64>>::with_capacity(2000000000);
     let mut w_le = <BufBitWriter<LE, _>>::new(MemWordWriterVec::new(v_le));
-    let v_be = <Vec<u64>>::with_capacity(1000000000);
+    let v_be = <Vec<u64>>::with_capacity(2000000000);
     let mut w_be = <BufBitWriter<BE, _>>::new(MemWordWriterVec::new(v_be));
     let mut r = SmallRng::seed_from_u64(0);
 
@@ -20,14 +21,18 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         b.iter(|| black_box(r.next_u64().trailing_zeros() as u64))
     });
 
-    c.bench_function("write_unary<LE>", |b| {
-        b.iter(|| w_le.write_unary(black_box(r.next_u64().trailing_zeros() as u64)))
-    });
-
     c.bench_function("write_unary<BE>", |b| {
         b.iter(|| w_be.write_unary(black_box(r.next_u64().trailing_zeros() as u64)))
     });
+
+    c.bench_function("write_unary<LE>", |b| {
+        b.iter(|| w_le.write_unary(black_box(r.next_u64().trailing_zeros() as u64)))
+    });
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group! {
+    name = benches;
+    config = Criterion::default().warm_up_time(Duration::from_secs(1)).measurement_time(Duration::from_secs(3));
+    targets = criterion_benchmark
+}
 criterion_main!(benches);
