20862cc449b8cf82a69de419993cfa8e7483e0ea
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2025-03-17 10:55:48 +0100

    Copyright blurbs

--- src/dispatch/factory.rs
+++ src/dispatch/factory.rs
@@ -1,4 +1,12 @@
-use super::*;
+/*
+ * SPDX-FileCopyrightText: 2025 Tommaso Fontana
+ * SPDX-FileCopyrightText: 2025 Inria
+ * SPDX-FileCopyrightText: 2025 Sebastiano Vigna
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR LGPL-2.1-or-later
+ */
+
+ use super::*;
 use anyhow::Result;
 use core::fmt::Debug;
 
