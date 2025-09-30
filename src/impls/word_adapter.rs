7a7a80ca685e18353ab97ce20ad99dec8a65df71
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2025-09-30 15:16:10 +0200

    Better error message

--- src/impls/word_adapter.rs
+++ src/impls/word_adapter.rs
@@ -69,8 +69,8 @@ impl<W: UnsignedInt + ToBytes + FromBytes + FiniteRangeNumber, B: Read> WordRead
                         e.kind(),
                         format!(concat!(
                             "Unexpected end of file. ",
-                            "This might happen because the file length is not a multiple of the word size ({}). ",
-                            "In this case, please pad with zeros at the end of the file. ",
+                            "This might happen because the file length is not a multiple of the word size used for reading ({} bytes). ",
+                            "In this case, please pad with zeros at the end of the file so that the file length is a multiple of {0}. ",
                             "The inner std::io::Error was {:?}"), W::BYTES, e),
                     )
                 }
